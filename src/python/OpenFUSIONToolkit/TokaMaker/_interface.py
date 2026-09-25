610f259e33c7d29f076d4bc027bb815d0d458e7c
John L <j42558080+1@gmail.com>
2026-09-24 20:32:22 -0400

    Fix f2py func signature for multistep

--- src/python/OpenFUSIONToolkit/TokaMaker/_interface.py
+++ src/python/OpenFUSIONToolkit/TokaMaker/_interface.py
@@ -81,7 +81,7 @@ tokamaker_init_psi = ctypes_subroutine(oftpy_lib.tokamaker_init_psi,
 tokamaker_solve = ctypes_subroutine(oftpy_lib.tokamaker_solve,
     [c_void_p, c_bool, c_int, c_int_ptr, c_char_p])
 
-# tokamaker_multistep(tMaker_ptr,vacuum,nl_its,error_str)
+# tokamaker_multistep(tMaker_ptr,error_str)
 tokamaker_multistep = ctypes_subroutine(oftpy_lib.tokamaker_multistep,
     [c_void_p, c_char_p])
 
